1ea2286cf718ad219e7c11332a2838af8b11ec78
Rachael Passov <rachael.passov@vertical-knowledge.com>
2016-05-29 18:07:28 -0400

    basic favicon

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const getConfig = require('hjs-webpack');
 const root    = resolve(__dirname);
 const src     = join(root, 'src');
 const modules = join(root, 'node_modules');
-const dist    = join(root, 'dist');\
+const dist    = join(root, 'dist');
 
 const NODE_ENV = process.env.NODE_ENV;
 const isDev = NODE_ENV === 'development';
@@ -18,7 +18,16 @@ var config = getConfig({
   isDev: isDev,
   in: join(src, 'app.js'),
   out: dist,
-  clearBeforeBuild: true
+  clearBeforeBuild: '!(img|favicon.ico)',
+  html: function(context) {
+    return {
+      'index.html': context.defaultTemplate({
+        title: 'React Yelp Clone',
+        head: '<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">\
+               <link rel="icon" href="/favicon.ico" type="image/x-icon">'
+      })
+    }
+  }
 });
 
 module.exports = config;
